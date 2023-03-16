2b9bed324859ca370a0f526edeb5b9b5f356901c
chairul <chairul.amri@ebconnect.com>
2023-03-16 18:05:16 +0700

    Learn custom TextField Widget

--- textfield_example/lib/main.dart
+++ textfield_example/lib/main.dart
@@ -27,6 +27,35 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               TextField(
+                decoration: InputDecoration(
+                  //Widget yang akan mendekorasi [TextField]
+                  filled: true, //Aktivasi fiil pada [TextField]
+                  fillColor: Colors.blue[50],
+                  icon: Icon(Icons
+                      .add_alert), //Icon yang akan muncul diluar [TextField]
+                  // prefix: Container( //Widget yang muncul sebelum Value dari [TextField]
+                  //   width: 5
+                  //   height: 5,
+                  //   color: Colors.red,
+                  // ),
+                  suffix: Container(
+                    //Widget yang muncul setelah Value dari [TextField]
+                    width: 5,
+                    height: 5,
+                    color: Colors.red,
+                  ),
+                  prefixIcon: Icon(Icons.person),
+                  prefixText: "Name: ",
+                  prefixStyle: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.w700),
+                  labelText:
+                      "Nama Lengkap", //Widget yang akan tampil diatas [TextField]
+                  hintText: "Nama Lengkapnya Loh",
+                  hintStyle: TextStyle(fontSize: 12),
+                  border: OutlineInputBorder(
+                      //Widget yang menampilkan border dari [TextField]
+                      borderRadius: BorderRadius.circular(12)),
+                ),
                 // obscureText: true,  //Jenis input Password
                 maxLength: 10, //Maximum panjang karakter
                 onChanged: (value) {
