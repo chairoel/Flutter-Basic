863f8c1e9381f559dbfd06471ce86f3aa3aad970
chairul <chairul.amri@ebconnect.com>
2023-03-17 14:56:58 +0700

    Displays data with a responsive layout when rotated

--- demo_project/lib/main.dart
+++ demo_project/lib/main.dart
@@ -21,16 +21,45 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Latihan Media Query"),
-      ),
-      body: Container(
+        appBar: AppBar(
+          title: Text("Latihan Media Query"),
+        ),
+        // body: Container(
+        //   color: Colors.red,
+        //   //Membuat ukuran [width] pada widget [Container] menjadi 1/3 bagian dari layar
+        //   width: MediaQuery.of(context).size.width / 3,
+        //   //Membuat ukuran [height] pada widget [Container] menjadi 1/2 bagian dari layar
+        //   height: MediaQuery.of(context).size.height / 2,
+        // ),
+
+        body: Center(
+          child: (MediaQuery.of(context).orientation == Orientation.portrait)
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: generateContainers)
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: generateContainers),
+        ));
+  }
+
+  List<Widget> get generateContainers {
+    return <Widget>[
+      Container(
         color: Colors.red,
-        //Membuat ukuran [width] pada widget [Container] menjadi 1/3 bagian dari layar
-        width: MediaQuery.of(context).size.width / 3,
-        //Membuat ukuran [height] pada widget [Container] menjadi 1/2 bagian dari layar
-        height: MediaQuery.of(context).size.height / 2,
+        width: 100,
+        height: 100,
       ),
-    );
+      Container(
+        color: Colors.green,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100,
+      ),
+    ];
   }
 }
