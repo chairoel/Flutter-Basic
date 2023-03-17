5c5bb99109b2cd5446226f9b2d762305c40a7290
chairul <chairul.amri@ebconnect.com>
2023-03-17 20:18:23 +0700

    Moved the main page class to a separate file

--- demo_project/lib/main.dart
+++ demo_project/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo_project/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,57 +10,57 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MainPage(),
     );
   }
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+// class MainPage extends StatelessWidget {
+//   const MainPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Media Query"),
-        ),
-        // body: Container(
-        //   color: Colors.red,
-        //   //Membuat ukuran [width] pada widget [Container] menjadi 1/3 bagian dari layar
-        //   width: MediaQuery.of(context).size.width / 3,
-        //   //Membuat ukuran [height] pada widget [Container] menjadi 1/2 bagian dari layar
-        //   height: MediaQuery.of(context).size.height / 2,
-        // ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("Latihan Media Query"),
+//         ),
+//         // body: Container(
+//         //   color: Colors.red,
+//         //   //Membuat ukuran [width] pada widget [Container] menjadi 1/3 bagian dari layar
+//         //   width: MediaQuery.of(context).size.width / 3,
+//         //   //Membuat ukuran [height] pada widget [Container] menjadi 1/2 bagian dari layar
+//         //   height: MediaQuery.of(context).size.height / 2,
+//         // ),
 
-        body: Center(
-          child: (MediaQuery.of(context).orientation == Orientation.portrait)
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: generateContainers)
-              : Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: generateContainers),
-        ));
-  }
+//         body: Center(
+//           child: (MediaQuery.of(context).orientation == Orientation.portrait)
+//               ? Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: generateContainers)
+//               : Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: generateContainers),
+//         ));
+//   }
 
-  List<Widget> get generateContainers {
-    return <Widget>[
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.green,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.blue,
-        width: 100,
-        height: 100,
-      ),
-    ];
-  }
-}
+//   List<Widget> get generateContainers {
+//     return <Widget>[
+//       Container(
+//         color: Colors.red,
+//         width: 100,
+//         height: 100,
+//       ),
+//       Container(
+//         color: Colors.green,
+//         width: 100,
+//         height: 100,
+//       ),
+//       Container(
+//         color: Colors.blue,
+//         width: 100,
+//         height: 100,
+//       ),
+//     ];
+//   }
+// }
